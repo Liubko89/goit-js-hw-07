383228102f238820edd3c83967c0c6237e75d536
Liubko89 <142455025+Liubko89@users.noreply.github.com>
2023-12-13 22:31:03 +0000

    task - 4

--- js/task-04.js
+++ js/task-04.js
@@ -1,1 +1,15 @@
+const loginForm = document.querySelector(".login-form");
 
+function formEvent(event) {
+  event.preventDefault();
+  const form = event.target;
+  const email = form.elements.email.value;
+  const password = form.elements.password.value;
+
+  if (email === "" || password === "") {
+    return alert("All form fields must be filled in");
+  }
+  form.reset();
+}
+
+loginForm.addEventListener("submit", formEvent);
