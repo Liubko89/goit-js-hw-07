68161f9c8eb055d97322115dae12e3fa33ad16b2
Liubko89 <142455025+Liubko89@users.noreply.github.com>
2023-12-15 21:39:54 +0000

    task - 5

--- js/task-05.js
+++ js/task-05.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.querySelector("body");
+const span = document.querySelector(".color");
+const btn = document.querySelector(".change-color");
+
+const changeColor = () => {
+  const rendomColor = getRandomHexColor();
+  body.style.backgroundColor = rendomColor;
+  span.textContent = rendomColor;
+};
+
+btn.addEventListener("click", changeColor);
