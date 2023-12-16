a1516c2818c773a1d571514b3dff532ac8ef7b1d
Liubko89 <142455025+Liubko89@users.noreply.github.com>
2023-12-16 00:47:02 +0000

    task - 6

--- js/task-06.js
+++ js/task-06.js
@@ -3,3 +3,36 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const btnCreate = document.querySelector("button[data-create]");
+const btnDestroy = document.querySelector("button[data-destroy]");
+const boxes = document.querySelector("#boxes");
+const input = document.querySelector("input");
+
+boxes.style.display = "flex";
+boxes.style.gap = "16px";
+
+const getEvents = (event) => {
+  const inputValue = event.target.value;
+
+  if (inputValue > 0) {
+    const create = () => {
+      boxes.innerHTML = "";
+      let size = 24;
+      for (let i = 0; i < inputValue; i += 1) {
+        boxes.insertAdjacentHTML(
+          "beforeend",
+          `<div style="background-color:${getRandomHexColor()};width:${size}px;height:${size}px;display:flex;flex-direction:row;"></div>`
+        );
+        size += 24;
+      }
+
+      input.value = "";
+      btnCreate.removeEventListener("click", create);
+    };
+
+    btnCreate.addEventListener("click", create);
+    btnDestroy.addEventListener("click", () => (boxes.innerHTML = ""));
+  }
+};
+
+input.addEventListener("input", getEvents);
